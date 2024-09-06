6d84b31ff2c90407ee78af5962c11808d7f097d2
Eric Carlisle <ecarlisle@users.noreply.github.com>
2024-09-06 15:14:22 -0400

    Update Astro config for Preact integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     open: "/",
     port: 3000,
   },
+
   devToolbar: {
     enabled: false
   },
+
+  integrations: [preact({
+    include: ['./src/components/preact/**/*.tsx'],
+  })],
 });
